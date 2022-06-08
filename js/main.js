84a1f9d81dc46f9a6ccbff6b1c8b8dc4a92d607a
Konrad <konrad.surowiec3@wp.pl>
2022-06-08 22:07:03 +0200

    added section's licence

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 const body = document.querySelector('body');
 
-//shadow modal
-const bgShadow = document.querySelector('.bg-shadow');
-
 //modal boxes
 const modal = document.querySelector('.modal');
 const modalBg = document.querySelector('.modal-bg');
@@ -12,105 +9,111 @@ const modalText = document.querySelector('.modal__text');
 const btnsRead = document.querySelectorAll('.offer__card-btn');
 
 //nav
-const burgerBtn = document.querySelector('.burger-btn');
+const burgerBtn = document.querySelector('.hamburger');
 const navMovile = document.querySelector('.nav-mobile');
 const navDesktop = document.querySelector('.nav-desktop');
 const navItemsMobile = document.querySelectorAll('.nav__item--mobile');
 
+// //section shopping
+// const btnsChoice = document.querySelectorAll('.shopping__card-btn');
+// const shadowCards = document.querySelectorAll('.shopping__card--shadow');
+
+// const brighteningCard = () => {
+//     shadowCards[0].classList.toggle('shopping__card--brightening');
+//     console.log('hej');
+// };
+
+// btnsChoice.forEach((btn) => {
+//     btn.addEventListener('mouseenter', brighteningCard);
+// });
+
 //show and close navigation mobile
 const handleNav = () => {
-    body.classList.toggle('fixed-body');
-    navMovile.classList.toggle('nav-mobile--active');
+	body.classList.toggle('fixed-body');
+	navMovile.classList.toggle('nav-mobile--active');
+	burgerBtn.classList.toggle('is-active');
 
-    navItemsMobile.forEach(item => {
-        item.addEventListener('click', () => {
-            body.classList.remove('fixed-body');
-            navMovile.classList.remove('nav-mobile--active');
-        })
-    })
-}
+	navItemsMobile.forEach((item) => {
+		item.addEventListener('click', () => {
+			body.classList.remove('fixed-body');
+			navMovile.classList.remove('nav-mobile--active');
+			burgerBtn.classList.remove('is-active');
+		});
+	});
+};
 //show and close modal
 const modalClose = () => {
 	modalBg.classList.remove('modal-bg--active');
 	modal.classList.remove('modal--active');
-    modalImg.src = '';
-    modalImg.alt = '';
+	modalImg.src = '';
+	modalImg.alt = '';
+	body.classList.remove('fixed-body');
 };
 
 const modalShow = () => {
 	modalBg.classList.add('modal-bg--active');
 	modal.classList.add('modal--active');
+	body.classList.add('fixed-body');
 };
 
 //handle modal
 btnsRead.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        if (btn.id === 'exp') {
-            modalImg.src = './img/offer/exp.png'; 
-            modalImg.alt = 'postać na polu walki';
-            modalText.innerText = "Funkcja niezwykle przydatna bez niej gra niezdatna, expa przybywa a potwórów ubywa";
-        }
-    
-        else if(btn.id === 'e2'){
-            modalImg.src = './img/offer/e2.png'; 
-            modalImg.alt =  'postać obok elity 2';
-            modalText.innerText = "Elitarne potwory nie starszne, pomyślał by kto?";
-        }
-        
-        else if(btn.id === 'hero'){
-            modalImg.src = './img/offer/heros.png';
-            modalImg.alt = 'postać obok herosa';
-            modalText.innerText = "Przechodził może gdzieś tędy heros? Drania muszę ubić nim świt nastanie!";
-        }
-        
-        else if(btn.id === 'task'){
-            modalImg.src = './img/offer/quest.png'; 
-            modalImg.alt = 'postać obok zleceniodwacy';
-            modalText.innerText = "Pomagam choć nie muszę, przez złoto chęć pragnienia szarpie mą duszę. A cóż to?";
-        }
-        
-        else if(btn.id === 'tunia'){
-            modalImg.src = './img/offer/tunia.png';
-            modalImg.alt = 'postać obok sprzedawcy';
-            modalText.innerText = "Robię zakupy, to moje hobby, to mnie cieszy. Handluje to moja praca, to moja płaca!";
-        }
-        
-        else if(btn.id === 'additives'){
-            modalImg.src = './img/offer/additives.png'; 
-            modalImg.alt = 'grafika pikselowej szafy';
-            modalText.innerText = "Dodatki? Ktoś coś mówił? Ja tu tylko sprzątam!";
-        }
-        
-        else if(btn.id === 'antycaptcha'){
-            modalImg.src = './img/offer/antycaptcha.png'; 
-            modalImg.alt = 'dwa przekreślone miecze';
-            modalText.innerText = "Nie straszna mi żadna trwoga, przeskoczę każdy płotek!";
-        }
-        
-        
-        else if(btn.id === 'deposit'){
-            modalImg.src = './img/offer/depozyt.png';
-            modalImg.alt = 'postać obok strażnika depozytu';
-            modalText.innerText = "Chowam bo się boję!";
-        }
-        
-        else if(btn.id === 'modern'){
-            modalImg.src = './img/offer/NI&SI.png';
-            modalImg.alt = 'stary oraz nowy interface';
-            modalText.innerText = "Epoka średniowiecza i współczesność są mi bliskie!";
-        }
-        modalShow();
-    })
+	btn.addEventListener('click', () => {
+		if (btn.id === 'exp') {
+			modalImg.src = './img/offer/exp.png';
+			modalImg.alt = 'postać na polu walki';
+			modalText.innerText =
+				'Funkcja niezwykle przydatna bez niej gra niezdatna, expa przybywa a potwórów ubywa';
+		} else if (btn.id === 'e2') {
+			modalImg.src = './img/offer/e2.png';
+			modalImg.alt = 'postać obok elity 2';
+			modalText.innerText = 'Elitarne potwory nie starszne, pomyślał by kto?';
+		} else if (btn.id === 'hero') {
+			modalImg.src = './img/offer/heros.png';
+			modalImg.alt = 'postać obok herosa';
+			modalText.innerText =
+				'Przechodził może gdzieś tędy heros? Drania muszę ubić nim świt nastanie!';
+		} else if (btn.id === 'task') {
+			modalImg.src = './img/offer/quest.png';
+			modalImg.alt = 'postać obok zleceniodwacy';
+			modalText.innerText =
+				'Pomagam choć nie muszę, przez złoto chęć pragnienia szarpie mą duszę. A cóż to?';
+		} else if (btn.id === 'tunia') {
+			modalImg.src = './img/offer/tunia.png';
+			modalImg.alt = 'postać obok sprzedawcy';
+			modalText.innerText =
+				'Robię zakupy, to moje hobby, to mnie cieszy. Handluje to moja praca, to moja płaca!';
+		} else if (btn.id === 'additives') {
+			modalImg.src = './img/offer/additives.png';
+			modalImg.alt = 'grafika pikselowej szafy';
+			modalText.innerText = 'Dodatki? Ktoś coś mówił? Ja tu tylko sprzątam!';
+		} else if (btn.id === 'antycaptcha') {
+			modalImg.src = './img/offer/antycaptcha.png';
+			modalImg.alt = 'dwa przekreślone miecze';
+			modalText.innerText =
+				'Nie straszna mi żadna trwoga, przeskoczę każdy płotek!';
+		} else if (btn.id === 'deposit') {
+			modalImg.src = './img/offer/depozyt.png';
+			modalImg.alt = 'postać obok strażnika depozytu';
+			modalText.innerText = 'Chowam bo się boję!';
+		} else if (btn.id === 'modern') {
+			modalImg.src = './img/offer/NI&SI.png';
+			modalImg.alt = 'stary oraz nowy interface';
+			modalText.innerText =
+				'Epoka średniowiecza i współczesność są mi bliskie!';
+		}
+		modalShow();
+	});
 });
 
 //navigation desktop
 window.addEventListener('scroll', () => {
-    if(window.scrollY > 250){
-        navDesktop.classList.add('nav-desktop--active');
-    } else {
-        navDesktop.classList.remove('nav-desktop--active');
-    }
-})
+	if (window.scrollY > 250) {
+		navDesktop.classList.add('nav-desktop--active');
+	} else {
+		navDesktop.classList.remove('nav-desktop--active');
+	}
+});
 
 //animation's offer tittle
 window.addEventListener('scroll', () => {
@@ -122,7 +125,6 @@ window.addEventListener('scroll', () => {
 	}
 });
 
-
-
 modalCross.addEventListener('click', modalClose);
+modalBg.addEventListener('click', modalClose);
 burgerBtn.addEventListener('click', handleNav);
